33b3e34ab871dccba93ed3579e02a3935926644b
iulian0585 <iam.constantinescu@gmail.com>
2023-10-19 14:41:22 +0300

    add sandbox account

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,31 @@
+module "sandbox_account" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "i.constantinescu+sandbox@levi9.com"
+    AccountName               = "Sandbox"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "i.constantinescu+sandbox@levi9.com"
+    SSOUserFirstName          = "Sandbox"
+    SSOUserLastName           = "Account"
+  }
+
+  account_tags = {
+    "Name" = "Sandbox"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Iulian Constantinescu"
+    change_reason       = "Sandbox account creation"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
+
 # module "dev_account" {
 #   source = "./modules/aft-account-request"
 
